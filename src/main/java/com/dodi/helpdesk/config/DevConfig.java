4e198d622d7c7e8ed80a857f6eb1f976f158a700
dodilacerda <dodilacerda@hotmail.com>
2022-12-15 16:37:08 -0300

    Perfil de desenvolvimento

---
+++ src/main/java/com/dodi/helpdesk/config/DevConfig.java
@@ -0,0 +1,29 @@
+package com.dodi.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.dodi.helpdesk.services.DBService;
+
+@Configuration
+//@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+//O @Value pega o valor da variável spring.jpa.hibernate.ddl-auto no application.properties
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
+	@Bean
+	public boolean instanciaDB() {
+		
+		if(value.equals("create")) {
+			this.dbService.instanciaDB();
+		}
+		return false;
+	}
+}
